329689e32bfe2b798625414b82a7dd5014b823e9
chahatjain2002 <chahatjain8094@gmail.com>
2024-09-11 16:11:56 +0530

    1. added UI to upload the file 2. added firebase config file 3. added packages as per the requirement 4. implemented the logic to save data in firestore database

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { addDoc, collection, writeBatch } from "firebase/firestore";
+import Papa from "papaparse";
+import { db } from "./firebase/firebase_config";
+import { useState } from "react";
 import './App.css';
 
 function App() {
+  const [csvFile, setCsvFile] = useState(null);
+
+  const handleChange = (e) => {
+    setCsvFile(e.target.files[0]);
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (!csvFile) return;
+
+    Papa.parse(csvFile, {
+      header: true,
+      complete: async (results) => {
+        const data = results.data;
+        try {
+          const batch = writeBatch(db);
+          const collectionRef = collection(db, "question-app");
+          data.forEach(async (row) => {
+            const docRef = await addDoc(collectionRef, row);
+            batch.set(docRef, row);
+          });
+          await batch.commit();
+          alert("Data saved successfully!");
+        } catch (error) {
+          console.error("Error saving data: ", error);
+        }
+      },
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="file"
+          name="file"
+          onChange={handleChange}
+          accept=".csv"
+        />
+        <button type="submit">Upload</button>
+      </form>
     </div>
   );
 }
